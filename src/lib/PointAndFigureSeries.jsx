77275009cef7be40fbb400c3755e384453be722e
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2015-08-04 21:44:40 -0500

    refactor P&F to use a separate function to calculate the data

--- src/lib/PointAndFigureSeries.jsx
+++ src/lib/PointAndFigureSeries.jsx
@@ -8,6 +8,7 @@ class PointAndFigureSeries extends React.Component {
 	constructor(props) {
 		super(props);
 		this.drawOnCanvas = this.drawOnCanvas.bind(this);
+		this.getColumns = this.getColumns.bind(this);
 	}
 	componentDidUpdate(prevProps, prevState, prevContext) {
 		// if (this.context.type !== "svg") this.drawOnCanvas();
@@ -31,63 +32,101 @@ class PointAndFigureSeries extends React.Component {
 	handleClick(idx) {
 		console.log(this.context.plotData[idx]);
 	}
-	render() {
-		// if (this.context.type !== "svg") return null;
+	getColumns() {
+		var { xScale, xAccessor, yScale, yAccessor, plotData } = this.context;
+		var width = xScale(xAccessor(plotData[plotData.length - 1]))
+			- xScale(xAccessor(plotData[0]));
 
-		var width = this.context.xScale(this.context.xAccessor(this.context.plotData[this.context.plotData.length - 1]))
-			- this.context.xScale(this.context.xAccessor(this.context.plotData[0]));
-
-		var columnWidth = (width / (this.context.plotData.length - 1));
+		var columnWidth = (width / (plotData.length - 1));
 
 		var anyBox, j = 0;
 		while (anyBox === undefined) {
-			if (this.context.plotData[j].close !== undefined) {
-				anyBox = this.context.plotData[j].boxes[0];
+			if (plotData[j].close !== undefined) {
+				anyBox = plotData[j].boxes[0];
 			}
 			j++;
 		}
 
-		var boxHeight = Math.abs(this.context.yScale(anyBox.open) - this.context.yScale(anyBox.close));
+		var boxHeight = Math.abs(yScale(anyBox.open) - yScale(anyBox.close));
 
-		var { stroke, fill, strokeWidth } = this.props;
-		var columns = this.context.plotData
+		var { stroke, fill, strokeWidth, className } = this.props;
+		var columns = plotData
 				.filter((d) => d.close !== undefined)
 				.map((d, idx) => {
-					var boxes = d.boxes.map((box, i) => {
-						var boxshape;
-						if (d.direction > 0) {
+					var boxes = d.boxes.map((box, i) => ({
+							strokeWidth: strokeWidth,
+							className: d.direction > 0 ? "up" : "down",
+							stroke:  d.direction > 0 ? stroke.up : stroke.down,
+							fill: d.direction > 0 ? fill.up : fill.down,
+							columnWidth: columnWidth,
+							boxHeight: boxHeight,
+							direction: d.direction,
+							open: yScale(box.open),
+							// y2: yScale(box.close),
+						})
+					);
+					return {
+						boxes: boxes,
+						direction: d.direction,
+						className: className,
+						transform: `translate(${ (xScale(xAccessor(d)) - (columnWidth / 2)) }, 0)`,
+					}
+						/*if (d.direction > 0) {
 							boxshape = (
 								<g key={idx + "-" + i}>
 									<line className="up" strokeWidth={strokeWidth} stroke={stroke.up} fill={fill.up}
-										x1={0} y1={this.context.yScale(box.open)} x2={columnWidth} y2={this.context.yScale(box.close)} />
+										x1={0} y1={yScale(box.open)} x2={columnWidth} y2={yScale(box.close)} />
 									<line className="up" strokeWidth={strokeWidth} stroke={stroke.up} fill={fill.up}
-										x1={0} y1={this.context.yScale(box.close)} x2={columnWidth} y2={this.context.yScale(box.open)} />
+										x1={0} y1={yScale(box.close)} x2={columnWidth} y2={yScale(box.open)} />
 								</g>
 								);
 						} else {
 							boxshape = (
 								<ellipse key={idx + "-" + i}
 									className="down" strokeWidth={strokeWidth} stroke={stroke.down} fill={fill.down}
-									cx={columnWidth / 2} cy={this.context.yScale((box.open + box.close) / 2)}
+									cx={columnWidth / 2} cy={yScale((box.open + box.close) / 2)}
 									rx={columnWidth / 2} ry={boxHeight / 2} />
 								);
 						}
 						return boxshape;
 					});
-					var debug = debugFlag
-						? <rect x={0} y={0} height={980} width={columnWidth} style={{ opacity: 0.1 }} onClick={this.handleClick.bind(this, idx)}/>
-						: null;
-					var col = (<g key={idx} className={this.props.className}
-									transform={"translate(" + (this.context.xScale(this.context.xAccessor(d)) - (columnWidth / 2)) + ", 0)"}>
+
+					var col = (<g key={idx} className={className}
+									transform={"translate(" + (xScale(xAccessor(d)) - (columnWidth / 2)) + ", 0)"}>
 									{boxes}
-									{debug}
 								</g>);
-					return col;
+					return col;*/
 				});
+		return columns;
+	}
+	render() {
+		// if (this.context.type !== "svg") return null;
+		var columns = this.getColumns();
+		var { stroke, fill, strokeWidth, className } = this.props;
 
 		return (
 			<g>
-				{columns}
+				{columns.map((col, idx) => (
+					<g key={idx} className={col.className} transform={col.transform}>
+						{col.boxes.map((box, i) => {
+							if (col.direction > 0)
+								return (
+									<g key={`${ idx }-${ i }`}>
+										<line className="up" strokeWidth={strokeWidth} stroke={stroke.up} fill={fill.up}
+											x1={0} y1={box.open} x2={box.columnWidth} y2={box.open - box.boxHeight} />
+										<line className="up" strokeWidth={strokeWidth} stroke={stroke.up} fill={fill.up}
+											x1={0} y1={box.open - box.boxHeight} x2={box.columnWidth} y2={box.open} />
+									</g>
+								)
+							return (
+								<ellipse key={`${ idx }-${ i }`}
+									className="down" strokeWidth={strokeWidth} stroke={stroke.down} fill={fill.down}
+									cx={box.columnWidth / 2} cy={box.open + box.boxHeight / 2}
+									rx={box.columnWidth / 2} ry={box.boxHeight / 2} />
+							);
+						})}
+					</g>
+				))}
 			</g>
 		);
 	}
