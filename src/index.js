41adf0882c46b1ba592973df2366c6e6816a8c15
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2015-09-08 07:31:30 -0500

    0.2.0-beta-1

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import axes from "./lib/axes";
 import tooltip from "./lib/tooltip";
 import helper from "./lib/helper";
 
-const version = "0.2.0-alpha-8";
+const version = "0.2.0-beta-1";
 
 export default {
 	ChartCanvas,
