0d8ca1bb1433dab859d098e14f1aa4664a98e552
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2016-02-25 11:36:44 +0000

    fix config to work on c9

--- webpack.config.watch.js
+++ webpack.config.watch.js
@@ -9,10 +9,11 @@ config.devtool = "cheap-module-eval-source-map";
 
 config.devServer = {
 	contentBase: path.join(__dirname, "docs"),
-	host: "localhost", // "10.0.0.106", "localhost",
-	port: 8090,
+	host: process.env.IP, // "10.0.0.106", "localhost"
+	port: process.env.PORT,
 };
 
+// open https://web-rrag.c9users.io/
 // config.plugins = config.plugins.slice(0, config.plugins.length - 3);
 
 config.output.publicPath = `http://${config.devServer.host}:${config.devServer.port}/${config.output.publicPath}`;
