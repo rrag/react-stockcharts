385060cf3c971d7ef1ff5d60e627128124e89a6d
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2016-10-01 06:45:53 -0500

    create es build

--- scripts/release.js
+++ scripts/release.js
@@ -4,7 +4,6 @@ var path = require("path");
 var root = path.join(__dirname, "..");
 
 var origPackage = fs.readFileSync(path.join(root, "package.json")).toString();
-var buildPackage
 
 try {
 	var pkg = JSON.parse(origPackage);
@@ -12,15 +11,14 @@ try {
 	delete pkg.scripts;
 	delete pkg.browserify;
 	pkg.main = "index.js";
-	pkg["jsnext:main"] = "index.js";
+	pkg["jsnext:main"] = "es/index.js";
 
-	buildPackage = JSON.stringify(pkg, null, 2);
+	const buildPackage = JSON.stringify(pkg, null, 2);
 
+	fs.writeFile(path.join(root, "build", "package.json"), buildPackage, function() {
+		console.log("CJS package.json file rendered");
+	});
 } catch (er) {
 	console.error("package.json parse error: ", er);
 	// process.exit(1);
 }
-
-fs.writeFile(path.join(root, "build", "package.json"), buildPackage, function() {
-	console.log("CJS package.json file rendered");
-});
