71e189f0ecbd10f567f22cb4a87866a140d107bc
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2016-11-22 09:29:20 -0600

    0.6.0-beta.9

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ import * as helper from "./lib/helper";
 import * as interactive from "./lib/interactive";
 import * as utils from "./lib/utils";
 
-const version = "0.6.0-beta.8";
+const version = "0.6.0-beta.9";
 
 export {
 	ChartCanvas,
