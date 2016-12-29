6ed7c17d28203a90146095edd3569c64ee55d62f
Marco Geri <m.geri1974@gmail.com>
2016-12-29 18:33:11 +0100

    Weighted Moving Average indicator (WMA)

---
+++ src/lib/indicator/algorithm/wma.js
@@ -0,0 +1,45 @@
+"use strict";
+
+import { sum } from "d3-array";
+
+import { slidingWindow } from "../../utils";
+import { WMA as defaultOptions } from "../defaultOptionsForComputation";
+
+export default function() {
+
+    var { windowSize, sourcePath } = defaultOptions;
+
+    function calculator(data)
+    {
+        var weight = windowSize*(windowSize+1)/2;
+
+        var waverage = slidingWindow()
+            .windowSize(windowSize)
+            .sourcePath(sourcePath)
+            .accumulator((values) => {
+                return sum(values, function(v, i) { return (i+1) * v} ) / weight;
+            });
+
+        return waverage(data);
+    }
+    calculator.undefinedLength = function() {
+        return windowSize;
+    };
+    calculator.windowSize = function(x) {
+        if (!arguments.length) {
+            return windowSize;
+        }
+        windowSize = x;
+        return calculator;
+    };
+
+    calculator.sourcePath = function(x) {
+        if (!arguments.length) {
+            return sourcePath;
+        }
+        sourcePath = x;
+        return calculator;
+    };
+
+    return calculator;
+}
