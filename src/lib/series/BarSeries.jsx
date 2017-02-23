5b896f95689e28519d385c7703be7912784e5772
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2017-02-22 21:53:56 -0600

    improve bar chart performance

--- src/lib/series/BarSeries.jsx
+++ src/lib/series/BarSeries.jsx
@@ -6,10 +6,14 @@ import GenericChartComponent from "../GenericChartComponent";
 import { getAxisCanvas } from "../GenericComponent";
 
 import StackedBarSeries, {
-	drawOnCanvasHelper,
-	svgHelper,
-	identityStack
+	// drawOnCanvasHelper,
+	drawOnCanvas2,
+	getBarsSVG2,
+	// svgHelper,
+	// identityStack
 } from "./StackedBarSeries";
+
+import { functor, last, head } from "../utils";
 
 class BarSeries extends Component {
 	constructor(props) {
@@ -18,15 +22,19 @@ class BarSeries extends Component {
 		this.drawOnCanvas = this.drawOnCanvas.bind(this);
 	}
 	drawOnCanvas(ctx, moreProps) {
-		const { xAccessor } = moreProps;
+		// const { xAccessor } = moreProps;
 
-		drawOnCanvasHelper(ctx, this.props, moreProps, xAccessor, identityStack);
-
+		// drawOnCanvasHelper(ctx, this.props, moreProps, xAccessor, identityStack);
+		const bars = getBars(this.props, moreProps);
+		drawOnCanvas2(this.props, ctx, bars);
 	}
 	renderSVG(moreProps) {
-		const { xAccessor } = moreProps;
+		// const { xAccessor } = moreProps;
 
-		return <g>{svgHelper(this.props, moreProps, xAccessor, identityStack)}</g>;
+		// return <g>{svgHelper(this.props, moreProps, xAccessor, identityStack)}</g>;
+
+		const bars = getBars(this.props, moreProps);
+		return <g>{getBarsSVG2(this.props, bars)}</g>;
 	}
 	render() {
 		const { clip } = this.props;
@@ -65,3 +73,52 @@ BarSeries.propTypes = {
 BarSeries.defaultProps = StackedBarSeries.defaultProps;
 
 export default BarSeries;
+
+/*
+ Initially, this program was using StackedBarSeries.getBars
+ to benefit from code reuse and having a single place that
+ contains the logic for drawing all types of bar charts
+ simple, grouped, horizontal, but turnes out
+ making it highly cuztimizable also made it slow for the
+ most simple case, a regular bar chart.
+ This function contains just the necessary logic
+ to create bars
+*/
+function getBars(props, moreProps) {
+	const { baseAt, fill, stroke, widthRatio, yAccessor } = props;
+	const { xScale, xAccessor, plotData, chartConfig: { yScale } } = moreProps;
+
+	const getFill = functor(fill);
+	const getBase = functor(baseAt);
+
+	const width = Math.abs(xScale(xAccessor(last(plotData))) - xScale(xAccessor(head(plotData))));
+	const bw = (width / (plotData.length - 1) * widthRatio);
+	const barWidth = Math.round(bw);
+	const offset = (barWidth === 1 ? 0 : 0.5 * bw);
+
+	const bars = plotData
+		.map(d => {
+			const yValue = yAccessor(d);
+			let y = yScale(yValue);
+
+			const x = Math.round(xScale(xAccessor(d)) - offset);
+			let h = getBase(xScale, yScale, d) - yScale(yValue);
+
+			if (h < 0) {
+				y = y + h;
+				h = -h;
+			}
+
+			return {
+				// type: "line"
+				x,
+				y,
+				height: h,
+				width: barWidth,
+				fill: getFill(d, 0),
+				stroke: stroke ? getFill(d, 0) : "none",
+			};
+		});
+
+	return bars;
+}
