bc017d39db9544ebdcf5bf0766a5a50afb1b72b3
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2017-06-15 20:27:45 -0500

    0.7.0-beta.2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export { default as GenericChartComponent } from "./lib/GenericChartComponent";
 export { default as GenericComponent } from "./lib/GenericComponent";
 export { default as BackgroundText } from "./lib/BackgroundText";
 
-export const version = "0.7.0-beta.1";
+export const version = "0.7.0-beta.2";
 
 /*
 // chart types & Series
