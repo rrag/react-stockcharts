7d73d460041e8c5c68a2c23c629250b574298a10
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2017-06-29 17:35:23 -0500

    Add default color for tooltip label

--- src/lib/tooltip/ToolTipTSpanLabel.jsx
+++ src/lib/tooltip/ToolTipTSpanLabel.jsx
@@ -4,11 +4,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ToolTipTSpanLabel(props) {
-	return <tspan className="react-stockcharts-tooltip-label" fill="#4682B4" {...props}>{props.children}</tspan>;
+	return <tspan className="react-stockcharts-tooltip-label" {...props}>{props.children}</tspan>;
 }
 
 ToolTipTSpanLabel.propTypes = {
 	children: PropTypes.node.isRequired,
+	fill: PropTypes.string.isRequired,
+};
+
+ToolTipTSpanLabel.defaultProps = {
+	fill: "#4682B4"
 };
 
 export default ToolTipTSpanLabel;
