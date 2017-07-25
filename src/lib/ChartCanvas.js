34819e5b3200a2b56da04fbf22d7d3ab5d1a664c
sokai116 <sokai116@gmail.com>
2017-07-25 16:31:12 +0800

    zoom event props should consists with pan event

--- src/lib/ChartCanvas.js
+++ src/lib/ChartCanvas.js
@@ -567,10 +567,13 @@ class ChartCanvas extends Component {
 		const { onLoadMore } = this.props;
 
 		this.triggerEvent("zoom", {
-			show: true,
+            xScale,
+            plotData,
+            chartConfig,
 			mouseXY,
 			currentCharts,
 			currentItem,
+			show: true,
 		}, e);
 
 		this.setState({
