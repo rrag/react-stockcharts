def05b8d439292d9b6d6dbfb2cbed418893be480
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2017-09-07 21:33:01 -0500

    0.7.0-beta.17

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export { default as GenericComponent } from "./lib/GenericComponent";
 export { default as BackgroundText } from "./lib/BackgroundText";
 export { default as ZoomButtons } from "./lib/ZoomButtons";
 
-export const version = "0.7.0-beta.16";
+export const version = "0.7.0-beta.17";
 
 /*
 // chart types & Series
