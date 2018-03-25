4bdb4d3aeb71edf009fcecb5cc0b02bdb893ac7f
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2018-03-25 14:11:05 -0500

    Resolved conflitcts from #431

--- src/lib/series/TriangleMarker.js
+++ src/lib/series/TriangleMarker.js
@@ -1,13 +1,20 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 import { hexToRGBA, functor } from "../utils";
 
 function Triangle(props) {
+
 	const {
-		className, stroke, strokeWidth,
-		opacity, fill, point, width
+		className, strokeWidth,
+		opacity, point, width
 	} = props;
+
+	const rotation = getRotationInDegrees(props, point);
+	if (rotation == null) return null;
+
+	const fillColor = getFillColor(props);
+	const strokeColor = getStrokeColor(props);
+
 	const w = functor(width)(point.datum);
 	const { x, y } = point;
 	const { innerOpposite, innerHypotenuse } = getTrianglePoints(w);
@@ -16,20 +23,38 @@ function Triangle(props) {
 		${x + (w / 2)} ${y + innerOpposite},
 		${x - (w / 2)} ${y + innerOpposite}
 	`;
+
 	return (
 		<polygon
 			className={className}
 			points={points}
-			stroke={stroke}
+			stroke={strokeColor}
 			strokeWidth={strokeWidth}
 			fillOpacity={opacity}
-			fill={fill}
+			fill={fillColor}
+	        transform={rotation != 0 ? `rotate(${ rotation }, ${ x }, ${ y })` : null}
+
 		/>
 	);
 }
 Triangle.propTypes = {
-	stroke: PropTypes.string,
-	fill: PropTypes.string.isRequired,
+	direction: PropTypes.oneOfType( [
+		PropTypes.oneOf( [
+			"top",
+			"bottom",
+			"left",
+			"right",
+			"hide"] ),
+		PropTypes.func
+	] ).isRequired,
+	stroke: PropTypes.oneOfType( [
+		PropTypes.string,
+		PropTypes.func
+	] ).isRequired,
+	fill: PropTypes.oneOfType( [
+		PropTypes.string,
+		PropTypes.func
+	] ).isRequired,
 	opacity: PropTypes.number.isRequired,
 	point: PropTypes.shape({
 		x: PropTypes.number.isRequired,
@@ -44,6 +69,7 @@ Triangle.propTypes = {
 	]).isRequired
 };
 Triangle.defaultProps = {
+	direction: "top",
 	stroke: "#4682B4",
 	strokeWidth: 1,
 	opacity: 0.5,
@@ -60,15 +86,28 @@ Triangle.drawOnCanvas = (props, point, ctx) => {
 	Triangle.drawOnCanvasWithNoStateChange(props, point, ctx);
 };
 Triangle.drawOnCanvasWithNoStateChange = (props, point, ctx) => {
+
 	const { width } = props;
 	const w = functor(width)(point.datum);
 	const { x, y } = point;
 	const { innerOpposite, innerHypotenuse } = getTrianglePoints(w);
+	const rotationDeg = getRotationInDegrees(props, point);
+
 	ctx.beginPath();
 	ctx.moveTo(x, y - innerHypotenuse);
 	ctx.lineTo(x + (w / 2), y + innerOpposite);
 	ctx.lineTo(x - (w / 2), y + innerOpposite);
 	ctx.stroke();
+
+	// TODO: rotation does not work
+	// example: https://gist.github.com/geoffb/6392450
+	if ( rotationDeg !== null && rotationDeg != 0 ) {
+		ctx.save();
+		ctx.translate(x, y);
+		ctx.rotate(rotationDeg * Math.PI / 180); // 45 degrees
+		ctx.fill();
+		ctx.restore();
+	}
 	ctx.fill();
 };
 export default Triangle;
@@ -80,4 +119,34 @@ function getTrianglePoints(width) {
 		innerOpposite,
 		innerHypotenuse
 	};
+}
+
+function getFillColor( props ) {
+	const { fill, point } = props;
+	return fill instanceof Function ? fill( point.datum ) : fill;
+}
+
+function getStrokeColor( props ) {
+	const { stroke, point } = props;
+	return stroke instanceof Function ? stroke( point.datum ) : stroke;
+}
+
+function getRotationInDegrees( props, point ) {
+	const { direction } = props;
+	const directionVal = direction instanceof Function ? direction( point.datum ) : direction;
+	if ( directionVal === "hide" ) return null;
+
+	let rotate = 0;
+	switch ( directionVal ) {
+		case "bottom":
+			rotate = 180;
+			break;
+		case "left":
+			rotate = -90;
+			break;
+		case "right":
+			rotate = 90;
+			break;
+	}
+	return rotate;
 }
