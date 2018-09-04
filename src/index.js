a2e4ea74058c22d8ba00a5bf905bff40069bdb92
Ragu Ramaswamy <ragunath.ramaswamy@gmail.com>
2018-09-03 22:31:40 -0500

    0.7.8

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export { default as GenericComponent } from "./lib/GenericComponent";
 export { default as BackgroundText } from "./lib/BackgroundText";
 export { default as ZoomButtons } from "./lib/ZoomButtons";
 
-export const version = "0.7.7";
+export const version = "0.7.8";
 
 /*
 // chart types & Series
