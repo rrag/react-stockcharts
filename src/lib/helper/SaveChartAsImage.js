45762297f3f7d16e68b4b2983f8d7ae6b5e3afbc
eaglux <eaglux@gmail.com>
2018-10-07 14:48:55 +0300

    fix SaveChartAsImage.js: 1. devicePixelRatio / backingStorePixelRatio for correct image scaling on Retina devices 2. use file-saver for correct image saving on IE/Edge/Safari 3. add canvg parameter for fallback when saving in IE11/Edge (IE has security bug when trying to save canvas with any svg image painted on). See https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/1015651/ or similar bugreports. See description for canvg parameter here: https://github.com/exupero/saveSvgAsPng (canvg - If canvg is passed in, it will be used to write svg to canvas. This will allow support for Internet Explorer) 4. Add paddings parameter to save functions

--- src/lib/helper/SaveChartAsImage.js
+++ src/lib/helper/SaveChartAsImage.js
@@ -1,69 +1,168 @@
+import saveAsPng from 'save-svg-as-png';
+import FileSaver from 'file-saver';
 
+// ==== save-svg-as-png hack ====
+//
+// saveAsPng.svgAsDataUri does not give us width/height in done parameter.
+// therefore, we use saveAsPng.prepareSvg and copy some code from saveAsPng
+// (svg encode).
+// We will remove this code and use saveAsPng.svgAsDataUri when this PR will
+// be accepted: https://github.com/exupero/saveSvgAsPng/pull/193
+const svgDocType = '<?xml version="1.0" standalone="no"?>' + 
+  '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ' + 
+    '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" [<!ENTITY nbsp "&#160;">]>';
 
-import saveAsPng from "save-svg-as-png";
-import { isDefined } from "../utils";
-
-const dx = 0;
-const dy = 0;
+function reEncode(data) {
+  data = encodeURIComponent(data);
+  data = data.replace(/%([0-9A-F]{2})/g, function(match, p1) {
+    var c = String.fromCharCode('0x' + p1);
+    return c === '%' ? '%25' : c;
+  });
+  return decodeURIComponent(data);
+}
+// ==== save-svg-as-png hack end ====
 
 const SaveChartAsImage = {
-	save(doc, container, background, cb) {
-		saveAsPng.svgAsDataUri(container.getElementsByTagName("svg")[0], {}, function(uri) {
-			// eslint-disable-next-line prefer-const
-			let image = new Image();
-			image.onload = function() {
+  save(doc, container, {background, cb, paddings, canvg}) {
+    const {
+      left: paddingLeft = 0,
+      top: paddingTop = 0,
+      right: paddingRight = 0,
+      bottom: paddingBottom = 0,
+    } = paddings || {};
 
-				// eslint-disable-next-line prefer-const
-				let canvas = doc.createElement("canvas");
-				canvas.width = image.width;
-				canvas.height = image.height;
+    const svgElement = container.getElementsByTagName('svg')[0];
+    //see comment on save-svg-as-png hack.
+    //we need width/height of svg here, because image.width/height is incorrect in some cases
+    saveAsPng.prepareSvg(svgElement, {}, function(svg, width, height) {
+      const image = new Image();
+      image.onload = function() {
+        const makeCanvas = () => {
+          const canvas = doc.createElement('canvas');
+          const context = canvas.getContext('2d');
 
-				// eslint-disable-next-line prefer-const
-				let context = canvas.getContext("2d");
+          // we should correctly scale canvas (for devices with Retina-like screen) 
+          const devicePixelRatio = window.devicePixelRatio || 1;
+          const backingStoreRatio =
+            context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio ||
+            1;
 
-				if (isDefined(background)) {
-					context.fillStyle = background;
-					context.fillRect(0, 0, canvas.width, canvas.height);
-				}
-				const canvasList = container.getElementsByTagName("canvas");
-				for (let i = 0; i < canvasList.length; i++) {
-					const each = canvasList[i];
-					if (isDefined(each)) {
-						const parent = each.parentNode.parentNode.getBoundingClientRect();
-						const rect = each.getBoundingClientRect();
-						context.drawImage(each, rect.left - parent.left + dx, rect.top - parent.top + dy);
-					}
-				}
+          const pixelRatio = devicePixelRatio / backingStoreRatio;
 
-				context.drawImage(image, dx, dy);
-				cb(canvas.toDataURL("image/png"));
-			};
-			image.src = uri;
-		});
-	},
-	saveWithWhiteBG(doc, container, cb) {
-		return this.saveWithBG(doc, container, "white", cb);
-	},
-	saveWithDarkBG(doc, container, cb) {
-		return this.saveWithBG(doc, container, "#303030", cb);
-	},
-	saveWithBG(doc, container, background, cb) {
-		return this.save(doc, container, background, cb);
-	},
-	saveChartAsImage(container) {
-		this.saveWithWhiteBG(document, container, function(src) {
-			const a = document.createElement("a");
-			a.setAttribute("href", src);
-			a.setAttribute("download", "Chart.png");
+          const canvasWidth = (width + paddingLeft + paddingRight) * pixelRatio;
+          const canvasHeight =
+            (height + paddingTop + paddingBottom) * pixelRatio;
 
-			document.body.appendChild(a);
-			a.addEventListener("click", function(/* e */) {
-				a.parentNode.removeChild(a);
-			});
+          canvas.width = canvasWidth;
+          canvas.height = canvasHeight;
+          if (pixelRatio !== 1) {
+            //don't scale context, hope source canvases are scaled by ChartCanvas code
+          }
 
-			a.click();
-		});
-	}
+          if (background != null) {
+            context.fillStyle = background;
+            context.fillRect(0, 0, canvasWidth, canvasHeight);
+          }
+          const canvasList = container.getElementsByTagName('canvas');
+          for (let i = 0; i < canvasList.length; i++) {
+            const each = canvasList[i];
+            if (each) {
+              const parent = each.parentNode.parentNode.getBoundingClientRect();
+              const rect = each.getBoundingClientRect();
+              context.drawImage(
+                each,
+                (rect.left - parent.left + paddingLeft) * pixelRatio,
+                (rect.top - parent.top + paddingTop) * pixelRatio
+              );
+            }
+          }
+
+          const imageWidth = image.width;
+          const imageHeight = image.height;
+          return [canvas, context, pixelRatio, imageWidth, imageHeight];
+        };
+
+        try {
+          const [
+            canvas,
+            context,
+            pixelRatio,
+            imageWidth,
+            imageHeight,
+          ] = makeCanvas();
+          context.drawImage(
+            image,
+            paddingLeft * pixelRatio,
+            paddingTop * pixelRatio,
+            imageWidth * pixelRatio,
+            imageHeight * pixelRatio
+          );
+          //check for security error bug in ie11. it will throw an error
+          //synchronously, and ie11 fallback will be used (in catch block)
+          canvas.toDataURL();
+
+          //in ie11, security bug will happen here (asynchronously).
+          //so, we can't handle it here, we handle it in code above (canvas.toDataURL)
+          canvas.toBlob(cb);
+        } catch (e) {
+          if (canvg) {
+            const [
+              canvas,
+              context,
+              pixelRatio,
+              imageWidth,
+              imageHeight,
+            ] = makeCanvas();
+            const canvasSvg = doc.createElement('canvas');
+            canvg(canvasSvg, svg, {
+              scaleWidth: imageWidth * pixelRatio,
+              scaleHeight: imageHeight * pixelRatio,
+            });
+            context.drawImage(
+              canvasSvg,
+              paddingLeft * pixelRatio,
+              paddingTop * pixelRatio
+            );
+
+            canvas.toBlob(cb);
+          } else {
+            throw new Error('Send canvg parameter (https://github.com/canvg/canvg) to handle ie11 security bug on canvas saving');
+          }
+        }
+      };
+
+      const uri = 'data:image/svg+xml;base64,' + window.btoa(reEncode(svgDocType + svg));
+      image.src = uri;
+    });
+  },
+
+  saveWithWhiteBG(container, paddings, cb) {
+    return this.saveWithBG(container, 'white', paddings, cb);
+  },
+  saveWithDarkBG(container, paddings, cb) {
+    return this.saveWithBG(container, '#303030', paddings, cb);
+  },
+  saveWithBG(container, background, paddings, cb) {
+    return this.save(document, container, { background, paddings, cb });
+  },
+  saveChartAsImage(container, background, paddings, cb) {
+    return this.saveChartAsImageWithOptions(container, {
+      background, paddings, cb
+    });
+  },
+  saveChartAsImageWithOptions(container, options) {
+    const cb = blob => {
+      FileSaver.saveAs(blob, 'Chart.png');
+      if (cb) {
+        cb();
+      }
+    };
+    return this.save(document, container, options);
+  },  
 };
 
 export default SaveChartAsImage;
