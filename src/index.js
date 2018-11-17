ed157bd4689cf3d187715306cff57effb8d06fa6
eaglux <eaglux@gmail.com>
2018-11-17 17:21:19 +0300

    Add reexport of internal components (required for correct import from es/index.js module, in webpack4 to make treeshaking work)

--- src/index.js
+++ src/index.js
@@ -7,25 +7,17 @@ export { default as GenericChartComponent } from "./lib/GenericChartComponent";
 export { default as GenericComponent } from "./lib/GenericComponent";
 export { default as BackgroundText } from "./lib/BackgroundText";
 export { default as ZoomButtons } from "./lib/ZoomButtons";
+export { getAxisCanvas, getMouseCanvas } from "./lib/GenericComponent";
+export * from "./lib/scale";
+export * from "./lib/series";
+export * from "./lib/axes";
+export * from "./lib/coordinates";
+export * from "./lib/tooltip";
+export * from "./lib/helper";
+export * from "./lib/utils";
+export * from "./lib/indicator";
+export * from "./lib/interactive";
+export * from "./lib/algorithm";
+export * from "./lib/annotation";
 
 export const version = "0.7.8";
-
-/*
-// chart types & Series
-import * as series from "./lib/series";
-import * as scale from "./lib/scale";
-
-import * as coordinates from "./lib/coordinates";
-import * as indicator from "./lib/indicator";
-import * as algorithm from "./lib/algorithm";
-
-import * as annotation from "./lib/annotation";
-
-import * as axes from "./lib/axes";
-import * as tooltip from "./lib/tooltip";
-import * as helper from "./lib/helper";
-
-import * as interactive from "./lib/interactive";
-import * as utils from "./lib/utils";
-
-*/
